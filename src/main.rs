31d6a8ccefa3d7af4bde1b712793852fe8dc9b5f
Salama Ashoush <salamaashoush@gmail.com>
2024-06-12 14:27:58 +0200

    gun follows player + cursor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::f32::consts::PI;
+
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-use bevy::math::vec3;
+use bevy::math::{vec2, vec3};
 use bevy::prelude::*;
-use bevy::window::close_on_esc;
+use bevy::render::camera;
+use bevy::window::{close_on_esc, PrimaryWindow};
 
 // Window
 const WW: f32 = 1200.0;
@@ -23,11 +26,16 @@ const BG_COLOR: (u8, u8, u8) = (197, 204, 184);
 struct GlobalTextureAtlasHandle(Option<Handle<TextureAtlasLayout>>);
 #[derive(Resource)]
 struct GlobalSpriteSheetHandle(Option<Handle<Image>>);
+#[derive(Resource)]
+struct CursorPosition(Option<Vec2>);
 // Player
 const PLAYER_SPEED: f32 = 2.0;
 // Components
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Gun;
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 enum GameState {
@@ -62,12 +70,18 @@ fn main() {
     // Custom resources
     .insert_resource(GlobalTextureAtlasHandle(None))
     .insert_resource(GlobalSpriteSheetHandle(None))
+    .insert_resource(CursorPosition(None))
     // Systems
     .add_systems(OnEnter(GameState::Loading), load_assets)
     .add_systems(OnEnter(GameState::GameInit), (setup_camera, init_world))
     .add_systems(
       Update,
-      (handle_player_input).run_if(in_state(GameState::InGame)),
+      (
+        handle_player_input,
+        update_gun_transform,
+        update_cursor_position,
+      )
+        .run_if(in_state(GameState::InGame)),
     )
     .add_systems(Update, close_on_esc)
     .run();
@@ -113,6 +127,18 @@ fn init_world(
     },
     Player,
   ));
+  commands.spawn((
+    SpriteSheetBundle {
+      texture: image_handle.0.clone().unwrap(),
+      atlas: TextureAtlas {
+        layout: texture_atlas.0.clone().unwrap(),
+        index: 2,
+      },
+      transform: Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
+      ..default()
+    },
+    Gun,
+  ));
   next_state.set(GameState::InGame);
 }
 
@@ -141,8 +167,49 @@ fn handle_player_input(
   if d_key {
     delta.x += 1.0;
   }
-  delta = delta.normalize();
-  if delta.is_finite() && (w_key || a_key || s_key || d_key) {
-    transform.translation += vec3(delta.x, delta.y, 0.0) * PLAYER_SPEED;
+  delta = delta.normalize_or_zero();
+  transform.translation += vec3(delta.x, delta.y, 0.0) * PLAYER_SPEED;
+}
+
+fn update_gun_transform(
+  cursor_position: Res<CursorPosition>,
+  player_query: Query<&mut Transform, With<Player>>,
+  mut gun_query: Query<&mut Transform, (With<Gun>, Without<Player>)>,
+) {
+  if player_query.is_empty() || gun_query.is_empty() {
+    return;
   }
+  let player_pos = player_query.single().translation.truncate();
+  let cursor_pos = match cursor_position.0 {
+    Some(cursor_pos) => cursor_pos,
+    None => player_pos,
+  };
+  let mut gun_transform = gun_query.single_mut();
+
+  let angle = (player_pos.y - cursor_pos.y).atan2(player_pos.x - cursor_pos.x) + PI;
+  gun_transform.rotation = Quat::from_rotation_z(angle);
+
+  let offset = 20.0;
+  let new_gun_pos = vec2(
+    player_pos.x + offset * angle.cos() - 5.0,
+    player_pos.y + offset * angle.sin() - 15.0,
+  );
+  gun_transform.translation = vec3(new_gun_pos.x, new_gun_pos.y, gun_transform.translation.z);
+}
+
+fn update_cursor_position(
+  window_query: Query<&Window, With<PrimaryWindow>>,
+  camera_query: Query<(&Camera, &GlobalTransform), With<Camera>>,
+  mut cursor_position: ResMut<CursorPosition>,
+) {
+  if window_query.is_empty() || camera_query.is_empty() {
+    cursor_position.0 = None;
+  }
+
+  let (camera, camera_transform) = camera_query.single();
+  let window = window_query.single();
+  cursor_position.0 = window
+    .cursor_position()
+    .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+    .map(|ray| ray.origin.truncate());
 }
